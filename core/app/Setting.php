f683632b04546c0293a222d6c13b2bab94a1c3f9
asadalarma <asadalarma@gmail.com>
2019-06-16 13:01:01 +0500

    working on settings model

--- core/app/Setting.php
+++ core/app/Setting.php
@@ -6,5 +6,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class Setting extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'value',
+        'key'
+    ];
+
+    public static function getItem($key){
+        if(Setting::checkKey($key)){
+            $value = self::query()->where('key','=',$key)->value('value');
+            return $value;
+        }
+        else
+            return raayeConfig($key);
+    }
+
 }
